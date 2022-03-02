3b159d2f12a3f49223de01994cc37ce06ef38869
Vinicius Maciel <vinimaciel01@gmail.com>
2022-03-01 21:31:04 -0300

    copiando projeto inicial

--- webscrapp_bb/acesso_bb.py
+++ webscrapp_bb/acesso_bb.py
@@ -4,155 +4,140 @@ Download e leitura dos extratos de conta corrente, poupança e cartões de créd
 Exporta a base em excel.
 """
 
-import time
-import os
+import sys
 import locale
 import datetime as dt
 import pandas as pd
+from dateutil.relativedelta import relativedelta
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import TimeoutException
 
-from lib import login
-from lib import nav_contas
-from lib import nav_cartao
-from lib import funcoes
-from helpers.data_functions import data_converte
+import utils.append as app
+print(sys.path)
+
+# from utils import append
+
+# from lib import nav_contas
+# from lib import nav_cartao
+
+# from navegacao.funcoes import base_export
+# from library.funcoes import base_export
+
+# from helpers.data_functions import data_converte
 
 # import config # COMENTAR DEPOIS DOS TESTES
 
-def acesso_bb(PATH_DOWNLOAD, AGENCIA, CONTA, SENHA=None):
+# def acesso_bb(path_download, agencia, conta, senha=None):
 
-    #####################
-    ##### PARAMETROS ####
-    #####################
-
-    dt1 = dt.datetime(2021, 12, 1).date()
-    dt2 = dt.datetime.today().date()
+#     dt1 = data_converte(
+#         dt.datetime(2021, 12, 1).date())
+#     dt2 = data_converte(
+#         dt.datetime.today().date())
     
-    # # para teste
-    # PATH_DOWNLOAD = config.path_download
-    # AGENCIA = config.agencia1
-    # CONTA = config.conta1
-    # SENHA = config.senha1
+#     ###############
+#     ##### MAIN ####
+#     ###############
     
-    ###############
-    ##### MAIN ####
-    ###############
+#     base = pd.DataFrame({})
+#     baseh = pd.DataFrame({})
+#     locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
-    WAIT = 20
-    base = pd.DataFrame({})
-    baseh = pd.DataFrame({})
-    locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+#     # Lista de meses desejados
+#     lista_meses = pd.date_range(dt1, dt2, freq='MS').tolist()
+#     lista_meses = [x.strftime("%b/%y") for x in lista_meses]
+#     lista_meses = [x.lower() for x in lista_meses]
 
-    # valida datas
-    dt1 = data_converte(dt1)
-    dt2 = data_converte(dt2)
+#     if dt2.date() == dt.datetime.today().date():
+#         prox = dt.datetime.strptime(lista_meses[-1], '%b/%y')
+#         if prox.month == 12:
+#             prox_month = 1
+#             prox_year = prox.year + 1
+#         else:
+#             prox_month = prox.month + 1
+#             prox_year = prox.year
+#         prox = dt.date(prox_year, prox_month, 1).strftime("%b/%y")
+#         lista_meses.append(prox)
 
-    # Lista de meses desejados
-    lista_meses = pd.date_range(dt1, dt2, freq='MS').tolist()
-    lista_meses = [x.strftime("%b/%y") for x in lista_meses]
-    lista_meses = [x.lower() for x in lista_meses]
+#     # Abre o navegador na página
+#     URL = 'https://www.bb.com.br/pbb/pagina-inicial#/'
+#     driver = webdriver.Chrome()
+#     driver.maximize_window()
+#     driver.set_page_load_timeout(20000)  # milisegundos
+#     wdw = WebDriverWait(driver, 20)
 
-    if dt2.date() == dt.datetime.today().date():
-        prox = dt.datetime.strptime(lista_meses[-1], '%b/%y')
-        if prox.month == 12:
-            PROX_MONTH = 1
-            prox_year = prox.year + 1
-        else:
-            PROX_MONTH = prox.month + 1
-            prox_year = prox.year
-        prox = dt.date(prox_year, PROX_MONTH, 1).strftime("%b/%y")
-        lista_meses.append(prox)
+#     while True:
+#         try:
+#             driver.get(URL)
+#         except TimeoutException:
+#             print('Timeout, reloading...')
+#             driver.refresh()
+#             continue
+#         else:
+#             break
 
-    # Abre o navegador na página
-    URL = 'https://www.bb.com.br/pbb/pagina-inicial#/'
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    driver.set_page_load_timeout(20000)  # milisegundos
+#     # Navega para a página de login
+#     locator = (By.XPATH, '//*[@id="acesseSuaconta"]')
+#     tag = wdw.until(ec.presence_of_element_located(locator)) 
+#     driver.execute_script("arguments[0].click();", tag)
 
-    WAIT = 20
-    wdw = WebDriverWait(driver, WAIT)
+#     #####################
+#     ### LOGIN TITULAR ###
+#     #####################
 
-    while True:
-        try:
-            driver.get(URL)
-        except TimeoutException:
-            print('Timeout, reloading...')
-            driver.refresh()
-            continue
-        else:
-            break
+#     print('Primeiro Titular')
+#     login.login(driver, agencia, conta, senha)
 
-    # Navega para a página de login
-    locator = (By.XPATH, '//*[@id="acesseSuaConta"]')
-    AcesseSuaConta = wdw.until(ec.presence_of_element_located(locator)) 
-    driver.execute_script("arguments[0].click();", AcesseSuaConta)
+#     # #######################
+#     # #### conta CORRENTE ###
+#     # #######################
 
-    #####################
-    ### LOGIN TITULAR ###
-    #####################
+#     # nav_contas.nav_corrente(driver)
+#     # agregados, agregados_header = nav_contas.nav_extrato_baixar(driver, lista_meses, path_download)
 
-    print('Primeiro Titular')
-    login.login(driver, AGENCIA, CONTA, SENHA)
+#     # base = base.append(agregados, ignore_index=True)
+#     # baseh = baseh.append(agregados_header, ignore_index=True)
 
-    #######################
-    #### CONTA CORRENTE ###
-    #######################
+#     # ########################
+#     # #### conta  POUPANÇA ###
+#     # ########################
 
-    nav_contas.nav_corrente(driver)
-    agregados, agregados_header = nav_contas.nav_extrato_baixar(driver, lista_meses, PATH_DOWNLOAD)
+#     # nav_contas.nav_poupanca(driver)
+#     # agregados, agregados_header = nav_contas.nav_extrato_baixar(driver, lista_meses, path_download)
 
-    base = base.append(agregados, ignore_index=True)
-    baseh = baseh.append(agregados_header, ignore_index=True)
+#     # base = base.append(agregados, ignore_index=True)
+#     # baseh = baseh.append(agregados_header, ignore_index=True)
 
-    ########################
-    #### CONTA  POUPANÇA ###
-    ########################
+#     # ###############
+#     # #### CARTÃO ###
+#     # ###############
 
-    nav_contas.nav_poupanca(driver)
-    agregados, agregados_header = nav_contas.nav_extrato_baixar(driver, lista_meses, PATH_DOWNLOAD)
+#     # nav_cartao.nav_cartao(driver)
+#     # agregados, agregados_header = nav_cartao.nav_cartao_baixar(driver, lista_meses, path_download)
 
-    base = base.append(agregados, ignore_index=True)
-    baseh = baseh.append(agregados_header, ignore_index=True)
+#     # base = base.append(agregados, ignore_index=True)
+#     # baseh = baseh.append(agregados_header, ignore_index=True)
 
-    ###############
-    #### CARTÃO ###
-    ###############
+#     # ##########################
+#     # #### TROCA DE TITULAR ####
+#     # ##########################
 
-    nav_cartao.nav_cartao(driver)
-    agregados, agregados_header = nav_cartao.nav_cartao_baixar(driver, lista_meses, PATH_DOWNLOAD)
+#     # if login.login_dependente(driver, '2º'):
 
-    base = base.append(agregados, ignore_index=True)
-    baseh = baseh.append(agregados_header, ignore_index=True)
+#     #     print('Troca de titular')
+#     #     #### CARTÃO ###
+#     #     nav_cartao.nav_cartao(driver)
+#     #     agregados, agregados_header = nav_cartao.nav_cartao_baixar(driver, lista_meses, path_download)
 
-    ##########################
-    #### TROCA DE TITULAR ####
-    ##########################
+#     #     base = base.append(agregados, ignore_index=True)
+#     #     baseh = baseh.append(agregados_header, ignore_index=True)
 
-    print('Troca de titular')
-    nome_titular = '2º'
-    existe_titular = login.login_dependente(driver, nome_titular)
-    
-    ###############
-    #### CARTÃO ###
-    ###############
+#     ############
+#     #### FIM ###
+#     ############
 
-    if existe_titular == True:
-        nav_cartao.nav_cartao(driver)
-        agregados, agregados_header = nav_cartao.nav_cartao_baixar(driver, lista_meses, PATH_DOWNLOAD)
-
-        base = base.append(agregados, ignore_index=True)
-        baseh = baseh.append(agregados_header, ignore_index=True)
-
-    ############
-    #### FIM ###
-    ############
-
-    driver.close()
-    return base, baseh
+#     driver.close()
+#     return base, baseh
