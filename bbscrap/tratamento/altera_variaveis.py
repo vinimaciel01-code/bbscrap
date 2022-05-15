60c781ca56b6f6dfe1ad7c8793d17f486608dad7
Vinicius Maciel <vinimaciel01@gmail.com>
2022-05-15 20:50:30 -0300

    altera var: corrige calculo do mes da próxima fatura

--- bbscrap/tratamento/altera_variaveis.py
+++ bbscrap/tratamento/altera_variaveis.py
@@ -17,24 +17,24 @@ def altera_variaveis(dados):
     """
     data_base = dados.copy()
 
-    # MES_REF
-    # troca "próxima fatura" pelo mes da fatura
+    # MES_REF: troca "próxima fatura" pelo mes da fatura
 
-    # calcula o último mes com fatura completa
+        # calcula o último mes com fatura completa
     temp = data_base.loc[data_base['tipo_conta'].isin(['Cartão']) & ~data_base['mes_ref'].str.contains('próxima fatura')]
     temp = temp[['conta', 'mes_ref']]
+    temp = temp.drop_duplicates(['conta','mes_ref'])
+    temp['mes_ref'] = [dt.datetime.strptime(x, '%b/%y') for x in temp['mes_ref']]
     temp = temp.groupby('conta').max()
 
-    # próxima fatura será igual ao mes seguinte
-    temp['mes_ref'] = temp['mes_ref'].apply(lambda x: dt.datetime.strptime(x, '%b/%y').date())
-    temp['mes_ref'] = temp['mes_ref'] + relativedelta(months=1)
+        # próxima fatura será igual ao mes seguinte
+    temp['mes_ref'] = [x + relativedelta(months=1) for x in temp['mes_ref']]
     temp['mes_ref'] = [dt.datetime.strftime(x, '%b/%y') for x in temp['mes_ref']]
     
-    # substitui
+        # substitui
     for index, row in temp.iterrows():
-        data_base.loc[(data_base.conta == index), 'mes_ref'] = row['mes_ref']
+        data_base.loc[(data_base.conta == index) & (data_base.mes_ref == 'próxima fatura'), 'mes_ref'] = row['mes_ref']
 
-    # converte para data
+        # converte para data
     data_base['mes_ref'] = [dt.datetime.strptime(x, '%b/%y').date() for x in data_base['mes_ref']]
 
     return data_base
